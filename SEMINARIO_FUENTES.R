e0dea2cbf53d638fbc353d2c3daf58c315504699
mariodelarena <mdd1009@alu.ubu.es>
2022-10-19 11:25:02 +0200

    Importamos el excel que contiene las altas hospitalarias nacionales en 2020.

---
+++ SEMINARIO_FUENTES.R
@@ -0,0 +1,7 @@
+library(readxl)
+Altas_Hospitalarias <- read_excel("C:/Users/super/OneDrive/Escritorio/Curso 2022-2023/1º Semestre/Fuentes de Datos Biomédicas y Web Semántica/SEMINARIO_FUENTES/INPUT/DATA/Altas_Hospitalarias.xls")
+
+Altas_Hospitalarias
+str(Altas_Hospitalarias)
+summary(Altas_Hospitalarias)
+View(Altas_Hospitalarias)
