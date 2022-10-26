97be31d226ba52bd2c287da31e45db1dbf9ae662
mariodelarena <mdd1009@alu.ubu.es>
2022-10-26 10:14:59 +0200

    Actualización

--- SEMINARIO_FUENTES.R
+++ SEMINARIO_FUENTES.R
@@ -2,8 +2,6 @@ library(readxl)
 Altas_Hospitalarias <- read_excel("C:/Users/super/OneDrive/Escritorio/Curso 2022-2023/1º Semestre/Fuentes de Datos Biomédicas y Web Semántica/SEMINARIO_FUENTES/INPUT/DATA/Altas_Hospitalarias.xls")
 
 Altas_Hospitalarias
-str(Altas_Hospitalarias)
-summary(Altas_Hospitalarias)
 View(Altas_Hospitalarias)
 
 Calidad_Aire_Palma <- read_excel("INPUT/DATA/Calidad_Aire_Palma.xls")
