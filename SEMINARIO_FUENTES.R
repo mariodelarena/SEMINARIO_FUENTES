d5a574fa54dd7230235b57829f2a7cd5febe6093
mariodelarena <mdd1009@alu.ubu.es>
2022-11-02 09:34:57 +0100

    Actualización de altas hospitallarias

--- SEMINARIO_FUENTES.R
+++ SEMINARIO_FUENTES.R
@@ -1,5 +1,5 @@
 library(readxl)
-Altas_Hospitalarias <- read_excel("C:/Users/super/OneDrive/Escritorio/Curso 2022-2023/1º Semestre/Fuentes de Datos Biomédicas y Web Semántica/SEMINARIO_FUENTES/INPUT/DATA/Altas_Hospitalarias.xls")
+Altas_Hospitalarias <- read_excel("INPUT/DATA/Altas_Hospitalarias.xls")
 
 Altas_Hospitalarias
 View(Altas_Hospitalarias)
