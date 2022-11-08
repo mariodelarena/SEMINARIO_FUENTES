0c3b6372623aca86b2dd04128c36cb56c36c7e7b
eirarodriguez <erm1009@alu.ubu.es>
2022-11-08 20:45:22 +0100

    Ajuste de tablas.

--- SEMINARIO_FUENTES.R
+++ SEMINARIO_FUENTES.R
@@ -4,24 +4,31 @@ library(dplyr)
 
 #Tabla de altas Hospitalarias
 Altas_Hospitalarias <- read_excel("INPUT/DATA/Altas_Hospitalarias.xls")
+Altas_Hospitalarias <- Altas_Hospitalarias[6:459, ]
+Altas_Hospitalarias <- Altas_Hospitalarias %>% select(1, 12, 16, 17, 21)
 View(Altas_Hospitalarias)
 
 #Tabla de la calidad del aire de la Palma
 Calidad_Aire_Palma2020 <- read_excel("INPUT/DATA/Calidad_Aire_Palma.xls")
+Calidad_Aire_Palma2020 <- Calidad_Aire_Palma2020 %>% select(-PERIODO_HI, -starts_with('FL'))
+
 View(Calidad_Aire_Palma2020)
 
 #Tabla de la calidad del aire de Gijón
 Calidad_Aire_Gijon <- read_delim("INPUT/DATA/Calidad_Aire_Gijon.csv", 
                                  delim = ";", escape_double = FALSE, trim_ws = TRUE)
+Calidad_Aire_Gijon <- Calidad_Aire_Gijon %>% select(-Estacion, -Titulo, -latitud, -longitud, -Periodo)
+
 View(Calidad_Aire_Gijon)
 
 #Tabla de la calidad del aire de Aragón
 Calidad_Aire_Aragon <- read_excel("INPUT/DATA/IGEAR Calidad del aire - estac_ica_data.xlsx")
+Calidad_Aire_Aragon <- Calidad_Aire_Aragon %>% select(-id, -dato_medido, -dato_medido_mm)
+
 View(Calidad_Aire_Aragon)
 
 #Tabla de la calidad del aire de Cantabria ya editada
 Calidad_Aire_Cantabria_Sin_Corte <- read_excel("INPUT/DATA/Calidad_Aire_Cantabria.xls")
-View(Calidad_Aire_Cantabria_Sin_Corte)
 Calidad_Aire_Cantabria2020 <- Calidad_Aire_Cantabria_Sin_Corte[286:297,]
 View(Calidad_Aire_Cantabria2020)
 
@@ -29,4 +36,7 @@ View(Calidad_Aire_Cantabria2020)
 Calidad_Aire_Madrid <- read_delim("INPUT/DATA/Calidad_Aire_Madrid.csv", 
                                   delim = ";", escape_double = FALSE, trim_ws = TRUE)
 Calidad_Aire_Madrid <- mutate(Calidad_Aire_Madrid, magnitud = case_when(magnitud == 1 ~ 'SO2',  magnitud == 6 ~ 'CO', magnitud == 7 ~ 'NO', magnitud == 8 ~ 'NO2', magnitud == 9 ~ 'PM2.5', magnitud == 10 ~ 'PM10', magnitud == 12 ~ 'NOx', magnitud == 14 ~ 'O3', magnitud == 20 ~ 'C7H8', magnitud == 22 ~ 'Carbon_Negro', magnitud == 30 ~ 'C6H6', magnitud == 42 ~ 'CHtot', magnitud == 44 ~ 'CHnoMet', magnitud == 431 ~ 'MPX'))
+Calidad_Aire_Madrid <- Calidad_Aire_Madrid %>% select(-ano, -provincia, -municipio, -estacion, -punto_muestreo, -starts_with('v'))
+#Calidad_Aire_Madrid$horas <- apply(Calidad_Aire_Madrid[ ,c(4:27)], 1, mean, na.rm = TRUE)
+
 View(Calidad_Aire_Madrid)
